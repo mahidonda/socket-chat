6d5df4b68a92f902e7fca1266c5ea1322de28d72
mahidonda <dcsm5b10@gmail.com>
2017-04-18 09:52:15 +0530

    chnage server

--- index.js
+++ index.js
@@ -11,36 +11,14 @@ app.get("/",function(req,res){
 
 io.sockets.on('connection',function(socket){
 	
-	socket.on('new user',function(data,callback){
-		
-		if(usernames.indexOf(data) != -1){
-			callback(false);
-		}else{
-			console.log(data);
-			callback(true); 
-			socket.username=data;
-			usernames.push(socket.username);
-			io.sockets.emit('userJoin',{msg:socket.username+" Is Joined Now....."});
-			updateUsernames();
-		}
-	});
 	
-	//update username
-	function updateUsernames(){
-		io.sockets.emit('usernames',usernames);
-	}
-	
-	//send message
-	socket.on('send message',function(data){
-		io.sockets.emit('new message',{msg:data, user:socket.username});
+	socket.on('sendData',function(data){
+		io.sockets.emit('testRecive',data+ " from server..!");
 	});
 
 
 	//disconnect user
 	socket.on('disconnect',function(data){
-		if(!socket.username) return;
-		usernames.splice(usernames.indexOf(socket.username),1);
-		io.sockets.emit('userLeave',{msg:socket.username+" Is Leave Chat....."});
-		updateUsernames();
+		
 	});
 });
